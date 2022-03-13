7277a759dbaf5b855ca97e8a1775a9d107c7aea1
Vass Zsolt <rtmzsolt@gmail.com>
2022-03-13 20:03:11 +0100

    Source clone cleaned and added some comment for the easier usage.

--- TopDownAnalyzer/TopDownAnalyzer.java
+++ TopDownAnalyzer/TopDownAnalyzer.java
@@ -7,21 +7,31 @@ import java.util.Scanner;
 import static java.lang.System.out;
 
 public class TopDownAnalyzer {
-
+    /**
+     * The program determines whether the world input
+     * can be derived from the rules.
+     * If the word can be derived, the program writes the derivation steps to the console.
+     */
     public static String input;
 
-    public static String getInput() {
+    public static String getInput() { //Reading from the Console
         Scanner read=new Scanner(System.in);
-        input=read.nextLine(); //"For example: ba#S-TS|T,T-a|b"
-        //String input="ab#S-TS|T,T-a|b";
-        //String input="qwerty#S-qweZ,Z-Fty,F-r";
-        //String input="qwert#S-abc|qweZ,Z-abc";
+        input=read.nextLine();
+        //Example0 ba#S-TS|T,T-a|b"
+        //Example1 "ab#S-TS|T,T-a|b";
+        //Example2 "qwerty#S-qweZ,Z-Fty,F-r";
+        //Example3 "qwert#S-abc|qweZ,Z-abc";
+
+        if (input.contains("+")) { //Only needed for school assignments
+            input=input.replaceAll("\\+","");
+        }
 
         return input;
     }
-    public static List<String> getAllRules(String s) { //s contains the input in a same format (word#S-TS|T,T-a|b)
+    public static List<String> getAllRules(String s) {
+        //s contains the input in a same format (word#S-TS|T,T-a|b) and return a list which contains the all the rules
         List<String> rules=new ArrayList<>();
-        String[] rawRules=s.split("#"); //separate word and rules
+        String[] rawRules=s.split("#"); //separate input to word and rules
         rawRules=rawRules[1].split(","); //contains the raw rules like (S-TS|T)
         char tempNonTerminal;
 
@@ -36,12 +46,12 @@ public class TopDownAnalyzer {
         }
         return rules;
     }
-    public static String getWord(String s){ //s contains the input in a same format (word#S-TS|T,T-a|b)
+    public static String getWord(String s){ //s contains the input in a same format (word#S-TS|T,T-a|b) and return the word
         String [] rawInput=s.split("#"); //separate the input to word and rules
         return rawInput[0];
     }
     public static List<String> getRulesFor(char nonTerminal) {
-
+        //gets a non-terminal letter and return the rules which can be derived from the non-terminal
         List <String> rules = getAllRules(input);
         List<String> rulesForNonTerminal=new ArrayList<>(); //only contains the possible rules for nonTerminal
         for (String rule : rules) {
@@ -51,22 +61,20 @@ public class TopDownAnalyzer {
         }
         return rulesForNonTerminal;
     }
-
-
-
     public static String applyThisRuleFor(int ruleNumber,int indexOfNonTerminal,char nonTerminal, String generatedWord){
+        //retrieves non-terminal and the number of rule and if possible, returns the generated word after using the rule
         List<String> rules=getRulesFor(nonTerminal);
         if(ruleNumber>=rules.size())
         {
-            out.println("NEM LEVEZETHETŐ, mert nincs rá szabály");
+            out.println("The input word can't be derived because there are no rule for it");
             return generatedWord="";
         }
         else {
-            String eleje="", közepe="", vége="";
-            vége=generatedWord.substring(indexOfNonTerminal+1);
-            közepe=rules.get(ruleNumber).substring(2);
-            eleje=generatedWord.substring(0,indexOfNonTerminal);
-            generatedWord=eleje+közepe+vége;
+            String firstPartOfWord, generatedPartOfWord, lastPartOfWord;
+            lastPartOfWord=generatedWord.substring(indexOfNonTerminal+1);
+            generatedPartOfWord=rules.get(ruleNumber).substring(2);
+            firstPartOfWord=generatedWord.substring(0,indexOfNonTerminal);
+            generatedWord=firstPartOfWord+generatedPartOfWord+lastPartOfWord;
         }
         return generatedWord;
     }
@@ -76,58 +84,61 @@ public class TopDownAnalyzer {
         out.println("This program is only work with the right formatted input.");
         out.println("Please write an input word and the substitution rules in this format: word#S-TS|T,T-a|b");
 
-        char StartSymbol='S';
-        List <String> memory=new ArrayList<>();
+        char StartSymbol='S'; //In basic terminology it's S, but it can be everything else
+        List <String> memory=new ArrayList<>(); //Includes derivation steps
         String generatedWord=String.valueOf(StartSymbol);
         memory.add(generatedWord);
 
-
-
-
         String word=getWord(getInput());
-        int index=0;
+        int index=0; //progress in thw word to be generated
         int numberOfRule=0;
-        char lastNonTerminal='?';
+        char lastNonTerminal='?'; //the simplest solution for backTrack
 
         while(!word.equals(generatedWord)) {
-
                 if(lastNonTerminal!=generatedWord.charAt(index))
                 {
                     numberOfRule=0;
                 }
 
-                if (generatedWord.charAt(index) > 64 && generatedWord.charAt(index) < 91) { //It's a non terminal letter (capital letter)
+                if (isNonTerminal(generatedWord, index)) { //It's a non terminal letter (capital letter)
                     lastNonTerminal=generatedWord.charAt(index);
                     generatedWord=applyThisRuleFor(numberOfRule,index,lastNonTerminal,generatedWord);
-                    if(!memory.contains(generatedWord)) {
+                    if(!memory.contains(generatedWord)) { //add last step of derivation to the memory
                         memory.add(generatedWord);
                     }
-                    if(generatedWord.length()==0) {
+                    if(generatedWord.length()==0) {//If the word can't be derived break the whole iteration
                         break;
                     }
-                    if(generatedWord.length()> word.length()|| ((generatedWord.charAt(index)>96 && generatedWord.charAt(index)<123) && generatedWord.charAt(index)!=word.charAt(index)))
+                    if(generatedWord.length()> word.length()||
+                    //shouldn't generate longer word than the original one
+                      (isTerminal(generatedWord, index) && generatedWord.charAt(index)!=word.charAt(index)))
+                    //and shouldn't get wrong terminal letter
                     {
                         memory.remove(memory.size()-1);
-                        generatedWord= memory.get(memory.size()-1);
+                        generatedWord= memory.get(memory.size()-1); //the backtrack (rollback)
                     }
                 }
 
-
-            if(generatedWord.charAt(index)==word.charAt(index)) {
+            if(generatedWord.charAt(index)==word.charAt(index)) { //in case of match go to the next letter
                 index++;
-                //out.println("Index now: "+index);
                 numberOfRule=0;
             }
             else
             {
                 numberOfRule++;
             }
-
         }
 
-        out.println("------------------------------");
         for (String s : memory) {
             out.println(s);
         }
+    }
+
+    private static boolean isTerminal(String generatedWord, int index) {//If it's a non-capital letter return true
+        return generatedWord.charAt(index) > 96 && generatedWord.charAt(index) < 123;
+    }
+
+    private static boolean isNonTerminal(String generatedWord, int index) { //If it's a capital letter return true
+        return generatedWord.charAt(index) > 64 && generatedWord.charAt(index) < 91;
     }
 }
